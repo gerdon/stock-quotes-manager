5f590e5d120758ee9c1d7e50b183dbbc1d191c72
Gerdon Mafra <gerdon_mafra@hotmail.com>
2020-09-20 21:16:05 -0300

    Criar método para registrar a aplicação no stock manager

--- src/main/java/br/com/inatel/stockquotesmanager/StockQuotesManagerApplication.java
+++ src/main/java/br/com/inatel/stockquotesmanager/StockQuotesManagerApplication.java
@@ -1,7 +1,13 @@
 package br.com.inatel.stockquotesmanager;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.inatel.stockquotesmanager.model.Manager;
 
 @SpringBootApplication
 public class StockQuotesManagerApplication {
@@ -10,13 +16,13 @@ public class StockQuotesManagerApplication {
 		SpringApplication.run(StockQuotesManagerApplication.class, args);
 	}
 	
-//	@PostConstruct
-//	void conectionStart() {
-//		RestTemplate rt = new RestTemplate();
-//		
-//		ResponseEntity<Manager> manager = rt.postForEntity("http://localhost:8080/notification", new Manager("localhost", 8081), Manager.class);
-//		
-//		System.out.println(manager.toString());
-//	}
+	@PostConstruct
+	void conectionRegisterStart() {
+		RestTemplate rt = new RestTemplate();
+		Manager manager =  new Manager("localhost", 8081);
+		
+		ResponseEntity<Object> response = rt.postForEntity("http://localhost:8080/notification", manager, Object.class);
+		System.out.println(response.getStatusCodeValue() +" - "+ response.getBody());
+	}
 
 }
